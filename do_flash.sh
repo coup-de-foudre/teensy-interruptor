ab50e63338f47308441c67e5023223bb88cfa1ba
meawoppl <meawoppl@gmail.com>
2017-06-10 22:39:52 -0700

    do_flash.sh

--- do_flash.sh
+++ do_flash.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
 ~/arduino-1.8.2/arduino --verbose --board teensy:avr:teensy31:speed=72 --verify turboencabulator/turboencabulator.ino
+
+read -p "Press enter to continue"
+
+~/arduino-1.8.2/arduino --verbose --board teensy:avr:teensy31:speed=72 --flash turboencabulator/turboencabulator.ino
