38bb672422fc9c08c33b6caa424c5bb7a9296c15
meawoppl <meawoppl@gmail.com>
2017-06-10 23:55:59 -0700

    Add tiny rng for volume control

---
+++ turboencabulator/entropy.h
@@ -0,0 +1,17 @@
+uint32_t _rng_state = millis()
+
+
+void init_rng() {
+  _rng_state = 75380540 - millis()
+  for (int i=0; i<100; i++)
+	tiny_prng()
+}
+
+uint32_t tiny_prng() {
+	uint32_t x = _rng_state;
+	x ^= x << 13;
+	x ^= x >> 17;
+	x ^= x << 5;
+	_rng_state = x;
+	return x;
+}
